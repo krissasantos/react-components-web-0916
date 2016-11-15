99ce5e052a0c76244427ac4b680fc440b7340e29
krissasantos <krissa.santos31@gmail.com>
2016-11-15 15:37:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much.";
+
+const Button = React.createClass({
+  render(){
+    return React.createElement('button', {}, 'Click Me');
+  }
+
+});
+
+const nestedButtons = React.createElement('div', {}, [
+  React.createElement(Button),
+  React.createElement(Button),
+  ]);
